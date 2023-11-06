757f54e599f30e589e55dc1f4ac6dc699793ba14
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-11-06 12:28:46 +0200

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,8 +12,9 @@ def number_of_subscribers(subreddit):
     module
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
+    respnse = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
