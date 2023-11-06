f3ae592cfcf118d92d6df2f3cb7d22bed9986af4
umumararungu <umcynthi@gmail.com>
2023-11-06 13:15:16 +0200

    done

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+1-main
+"""
+import requests
+import sys
+
+def top_ten(subreddit):
+    """
+    1-main
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'cynt user agent 1.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    posts = response.json().get('data').get('children')
+    for post in posts[:10]:
+        print(response.json().get('data').get('title'))
