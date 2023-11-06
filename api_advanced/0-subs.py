f61419818447631c9d781049ed2b0c62a9397bf0
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-11-06 10:52:37 +0200

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,3 +11,11 @@ def number_of_subscribers(subreddit):
     """
     module
     """
+    subs_number =requests.get("https://www.reddit.com/api/subscribe"
+                              .format(argv[1]))
+    subs_number = subs_number.json()
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(subs_number(sys.argv[1])))
