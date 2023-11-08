d95e6ea6eb7b8846de36983654f93336ff26a615
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-11-08 12:07:45 +0200

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,11 +5,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+
     """
     A function that queries the Reddit API for a given subreddit
     and returns the total number of subscribers. 
     Returns 0 if the subreddit is invalid
     """
+    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'cynt user agent 1.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
